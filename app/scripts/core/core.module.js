2cfb288addf6be8c01fefbb48fd777d086036aec
Sean Davidson <acatcalledanarchy@gmail.com>
2015-01-06 23:08:21 +0000

    Laid copy out in columns

--- app/scripts/core/core.module.js
+++ app/scripts/core/core.module.js
@@ -16,7 +16,7 @@
          */
         'angular-loading-bar', 'firebase', 'ngMap', 'toastr', 'angulartics', 'angulartics.google.analytics', 'mm.foundation', 'duScroll',
 
-        'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.poster'
+        'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.poster', 'com.2fdevs.videogular.plugins.buffering', 'info.vietnamcode.nampnq.videogular.plugins.youtube'
     ]);
 
 })();
--- app/scripts/showreel/showreel.controller.js
+++ app/scripts/showreel/showreel.controller.js
@@ -8,30 +8,17 @@
 		.module('app')
 		.controller('ShowreelCtrl', ShowreelCtrl);
 
-	ShowreelCtrl.$inject = ['$sce'];
-
-	function ShowreelCtrl($sce) {
+	function ShowreelCtrl() {
 
 		var vm = this;
 		vm.introCopy = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-	
-		////////////////////////
-
 		vm.videogular = {
 			sources: [
+				{src: 'https://www.youtube.com/watch?v=gi-wl43o3gc' }/*
 				{src: $sce.trustAsResourceUrl('http://www.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'},
 				{src: $sce.trustAsResourceUrl('http://www.videogular.com/assets/videos/videogular.webm'), type: 'video/webm'},
-				{src: $sce.trustAsResourceUrl('http://www.videogular.com/assets/videos/videogular.ogg'), type: 'video/ogg'}
-			],/*
-			tracks: [
-				{
-					src: 'http://www.videogular.com/assets/subs/pale-blue-dot.vtt',
-					kind: 'subtitles',
-					srclang: 'en',
-					label: 'English',
-					default: ''
-				}
-			],*/
+				{src: $sce.trustAsResourceUrl('http://www.videogular.com/assets/videos/videogular.ogg'), type: 'video/ogg'}*/
+			],
 			theme: 'bower_components/videogular-themes-default/videogular.css',
 			plugins: {
 				poster: 'http://www.videogular.com/assets/images/videogular.png'

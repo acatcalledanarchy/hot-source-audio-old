3e1078bde783be70d3ebc7a262541be86e33432a
Sean Davidson <acatcalledanarchy@gmail.com>
2015-01-06 08:58:43 +0000

    Removed Angular-Bootstrap-Calendar

--- app/scripts/core/core.module.js
+++ app/scripts/core/core.module.js
@@ -14,7 +14,7 @@
         /*
          * 3rd Party modules
          */
-        'angular-loading-bar', 'firebase', 'ngCart', 'ngMap', 'toastr', 'mwl.calendar', 'ui.bootstrap', 'toggle-switch','angulartics', 'angulartics.google.analytics'
+        'angular-loading-bar', 'firebase', 'ngCart', 'ngMap', 'toastr', 'toggle-switch','angulartics', 'angulartics.google.analytics'
     ]);
 
 })();

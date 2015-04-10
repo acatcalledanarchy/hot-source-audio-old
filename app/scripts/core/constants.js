ce4b30121f0522f87b0d9fd4f3e3f4b127f847c6
System Administrator <root@as-macbook.lan>
2015-04-10 19:55:39 +0100

    Updated with new copy

--- app/scripts/core/constants.js
+++ app/scripts/core/constants.js
@@ -31,39 +31,43 @@
                         KEYWORDS: 'audio, production, radio, tv, film, voiceover, podcasts, restoration, commercial, music, online, digital, conversion, dat, minidisc, reel, cassette'
                     }, 
                     BANNER: 'home.jpg', 
-                    INTRO_COPY: '<p>Hot Source is an independent audio production company based in Guernsey in the Channel Islands. We are passionate about defining your brand with creative audio that connects and engages emotionally with your customers producing successful results.</p>',
-                    HOW_WE_WORK_COPY: '<p>We like to chat with you, get an overview of where you are with your current marketing and identify objectives.</p>' +
-                        '<p>We like to agree the creative brief then go to work.</p>' +
-                        '<p>We work to your brief and price accordingly. Where possible we will supply audio examples and cost guides, making sure you understand what is achievable for the final budget. We will also present demos where appropriate.</p>' +
-                        '<p>If you already have an creative agency on board we can work to develop ideas with them or purely as a production house.</p>',
+                    INTRO_COPY: '<p>Hot Source Source is an independent audio production company based in Guernsey in the Channel Islands. ' +
+                                'We write words, music and sound design for radio and tv advertising, online video, film and digital media.</p>' +
+                                '<p>We craft audio that connects and engages brands with people.</p>',
+                    HOW_WE_WORK_COPY: '<p>Clearly we have sense of humour but we do take the business of producing creative audio seriously.</p>' +
+                                '<p>So, simply put....</p>' +
+                                '<p>If you’re a radio station sales manager we can arm your team with creative variety in commercial script writing, campaign ideas, music and production.</p>' +
+                                '<p>If you’re an advertising agency we can work alongside your creative team, take direction, advise and help bring their ideas to life across traditional and digital mediums.</p>' +
+                                '<p>If you’re a client direct we can design creative audio that can enhance your business from the tone of voice and music on your on hold messages to your online video blogs... ...spanning across radio and tv advertsing in between.</p>' +
+                                '<p>If you’re a film producer or videographer...we can write music you can feel with sound design you can see.</p>',
                     SERVICES: [
                         {
-                            TITLE: 'Radio commercial production',
+                            TITLE: 'Radio advertising',
                             IMAGE: 'studio.jpg',
-                            COPY: '<p>We write and produce radio commercials including scriptwriting, voice-over casting & recording, bespoke music branding.</p>',
+                            COPY: '<p>Radio Commercial Production including scriptwriting - voice-over casting & recording – music selection/composition</p>',
                             LINK: {
                                 PAGE_ANCHOR: 'radio-commericals-showreel',
                                 TEXT: 'Listen to showreel'
                             }
                         },
                         {
-                            TITLE: 'Music & voice for TV, Film',
+                            TITLE: 'Audio branding',
                             IMAGE: 'music-composition.jpg',
-                            COPY: '<p>Sound design and audio branding for TV commercials, bespoke music composition for film/documentary/digital video.</p>'
+                            COPY: '<p>Sound design, bespoke music and sonic logos, voice over selection and recording across radio, tv, film and digital content.</p>'
                         },
                         {
-                            TITLE: 'Music & voice online/digital',
+                            TITLE: 'Film, TV & digital',
                             IMAGE: 'amp.jpg',
-                            COPY: '<p>Copywriting, voice casting and recording, music and sound design for online video, podcasts and audio books.</p>',
+                            COPY: '<p>Bespoke music composition, sound design, recording and production for Film documentary, advertising, web video, podcasts and audio books.</p>',
                             LINK: {
                                 PAGE_ANCHOR: 'music-showreel',
                                 TEXT: 'Listen to showreel'
                             }
                         },
                         {
-                            TITLE: 'Media conversion/audio restoration',
+                            TITLE: 'Library',
                             IMAGE: 'audio-restoration.jpg',
-                            COPY: '<p>We offer audio restoration work on those long lost audio recordings of yester year using, reel to reel machines, cassette dubbing decks, Mini disc and DAT.</p>'
+                            COPY: '<p>The Hot Source music library offers music in many styles which can be customised for exclusive use for production companies.</p>'
                         }
                     ]
                 },
@@ -73,10 +77,11 @@
                         KEYWORDS: 'studio, ww2, german, naval, command, seeko-ki, isdn, real-time, audio, drum, vocal, booth, live, guitar, room, enigma, coding, machine'
                     },
                     BANNER: 'studio.jpg',
-                    INTRO_COPY: '<p>Our studios are situated in the former WW2 German Naval Command Communications Bunker built during the 1940/45 occupation. Code named Seeko-Ki it housed one of the legendary enigma coding machines.</p>' +
-                        '<p>Other than the two most essential pieces of kit (our ears and imagination) we have all the industry leading hardware and software at hand to create music and sound design bringing your productions to life.</p>' +
-                        '<p>Via ISDN we can link with studios for real-time audio connections across the globe making voice talent available to any producers working at our facilities.</p>' +
-                        '<p>A live drum room, vocal booth and live guitar room compliments the two main production studios.</p>'
+                    INTRO_COPY: '<p>Hot Source studios are situated in the former WW2 German Naval Command Communications Bunker built during the 1940/45 occupation. ' +
+                    'Code named Seeko-Ki it housed one of the legendary enigma coding machines.</p>' +
+                    '<p>The Germans were here 5 years... we’ve been here much longer.</p>' +
+                    '<p>Other than the two most essential pieces of kit (our ears and imagination) we have all the industry leading hardware and software at hand to create music and sound to bring your audio to life.</p>' +
+                    '<p>Via ISDN, Ipdtl, source connect we can link with studios for real-time audio connections across the globe making voice talent available to any producers working at our facilities.</p>'
                 },
                 ABOUT_US: {
                     META: {
@@ -84,9 +89,10 @@
                         KEYWORDS: ''
                     },
                     BANNER: 'about-us.jpg',
-                    INTRO_COPY: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' + 
-                    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ' +
-                    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+                    INTRO_COPY: '<p>Originally set up as Evolution Studios in 1992 most of the 90s saw founder Mark Adnitt collaborating with Nick Windsor on many campaigns for KWL (the agency Nick was a partner in at the time). ' +
+                    'At this time Mark also wrote and produced commercials for other local and uk radio stations and agencies ...particularly specialising in bespoke music for tv commercials for UK agencies.</p>' +
+                    '<p>Together they were responsible for the production for radio commercials and bespoke music that featured on local and national radio and tv stations and as far as Sydney.</p>' +
+                    '<p>On the formation of Hot Source and with the addition of Simon Prince they have since worked with UK agencies, radio stations and film companies writing words and music for notable brands including Emirates Airlines, Castrol GTX, 7-Up, Sea France, Specsavers International, British Airways, Fred Olsen and Orient Express.</p>'
                 },
                 OUR_WORK: {
                     META: {
@@ -119,23 +125,17 @@
             },
             EMPLOYEES: [
                 {
-                    FIRST_NAME: 'Simon',
-                    SURNAME: 'Prince',
-                    JOB_TITLE: 'Producer/composer/sound designer',
-                    EMAIL_ADDRESS: 'simon@hotsourceaudio.com',
-                    IMAGE: 'simon.jpg',
-                    TELEPHONE_NUMBER: '+44 (0) 1481 711111',
-                    COPY: '<p>A stickler for timing (he&rsquo;s also a highly respected drummer/percussionist) Simon left Leeds College of Music fully trained in studio engineering.</p>' +
-                        '<p>Now he works with creative writing and producing audio including voice casting, music branding and sound design across traditional and digital media.</p>'
-                },
-                {
                     FIRST_NAME: 'Mark',
                     SURNAME: 'Adnitt',
                     JOB_TITLE: 'Founding Partner – script writer/music composition/production',
                     EMAIL_ADDRESS: 'mark@hotsourceaudio.com',
                     IMAGE: 'mark.jpg',
                     TELEPHONE_NUMBER: '+44  (0) 1481 711112',
-                    COPY: '<p>Since converting the bunker to studios back in 1992 Mark has written and produced commercials for almost every business sector. His work has travelled far and wide... local radio, UK commercial radio and Television and more.</p><p>Clients value his experience, amiable character and his ability to catch many lobsters each summer. Ask him from where though and expect a stony silence.</p>'
+                    COPY: '<p>Mark moved to Guernsey in 1984 from London where he was a singer and producer working with bands in studios such as CTS and EMI Studios.</p>' +
+                    '<p>This experience was the stepping stone to a full-time career in commercial audio production that saw the birth of Evolution Studios in the early 90s.</p>' +
+                    '<p>Since then he has acquired over 20 years’ experience in writing and producing scripts for radio, and bespoke music for tv. His work has traversed the globe on local and national radio and tv stations as well as digital.</p>' +
+                    '<p>Throughout his career his sense of humour has been translated into many hundreds of radio commercials, music branding projects and advertising campaigns.</p>' +
+                    '<p>Clients value his experience, amiable character and his ability to catch many lobsters each summer. Ask him from where though and expect a stony silence.</p>'
                 },
                 {
                     FIRST_NAME: 'Nick',
@@ -144,7 +144,25 @@
                     EMAIL_ADDRESS: 'nick@hotsourceaudio.com',
                     IMAGE: 'nick.jpg',
                     TELEPHONE_NUMBER: '+4 (0) 1481 711113',
-                    COPY: '<p>Backed by years of experience in advertising and marketing in Guernsey, London and Australia clients count on Nick for insightful guidance on all aspects of their Marketing.</p><p>A true audio/visual person, experienced musician, composer, art director...he has written and consulted for well-respected local, national and global brands in on radio, TV and digital media.</p><p>Fellow creatives and clients feed off his passion and straight talking for the subject.</p>' }
+                    COPY: '<p>Nick brings a wealth of music composition and recording experience both as a session guitarist and writer.</p>' +
+                    '<p>He has played in semi-pro bands on big stages since he was 16, supporting the likes of Thin Lizzy, Sister Sledge and Big Country as well as playing guitar appearing alongside his pals in many other top local bands on stage and in the studio.</p>' +
+                    '<p>He has also toured with Paul Young, recorded with The Christians and performed at Ronnie Scotts.</p>' +
+                    '<p>Backed by years of experience marketing and advertising has enabled him to bring these skills to the fore when devising campaigns for radio advertising, tv soundtracks and bespoke music.</p>' +
+                    '<p>The need for a creative synergy between mediums and messages is a challenge he enjoys and one reason he is respected by fellow creatives and clients as a great sounding board for the bigger marketing picture.</p>'
+                },
+                {
+                    FIRST_NAME: 'Simon',
+                    SURNAME: 'Prince',
+                    JOB_TITLE: 'Producer/composer/sound designer',
+                    EMAIL_ADDRESS: 'simon@hotsourceaudio.com',
+                    IMAGE: 'simon.jpg',
+                    TELEPHONE_NUMBER: '+44 (0) 1481 711111',
+                    COPY: '<p>A stickler for timing Simon is highly respected as a drummer/percussionist and composer.</p>' +
+                    '<p>He attended Leeds College of Music where he earned a Diploma in studio engineering & Music Technology.</p>' +
+                    '<p>Since joining Hot Source he has been able to stretch his wings in engineering and production, bringing to life Hot Source and agency scripts with an unbridalled passion for sound design.</p>' +
+                    '<p>He has cast thousands of voices for commercials on radio, tv as well as digital content.</p>' +
+                    '<p>As a composer...his work has been heard on high profile companies both locally and globally.</p>'
+                }
             ],
             SHOWREEL: {
                 MP3S: [
